4791117c9bac36b17221478e088d9250d3116cd1
Kai Daguerre <kai@turbot.com>
2019-09-13 21:09:12 +0100

    update test config

--- config.tf
+++ config.tf
@@ -1,23 +1,9 @@
 resource "turbot_resource" "test" {
   parent = "tmod:@turbot/turbot#/"
   type = "tmod:@turbot/turbot#/resource/types/folder"
-  payload =
-  <<
-
-
-
-
-
-  EOF
+  payload = <<EOF
   {
-
-
-    "title": "provider_test2",
-
-
-    "description": "test resource2"
-
-
-  }
-  EOF
+  "title": "provider_test2",
+  "description": "test resource2"
+}EOF
 }
