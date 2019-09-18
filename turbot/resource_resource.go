086da493f19411adf5c99507c81952a9c57df856
Kai Daguerre <kai@turbot.com>
2019-09-18 19:06:38 +0530

    Bugfix: mapFromResourceData should use GetOk to determine if resourceData contains a property. Closes #24

--- turbot/resource_resource.go
+++ turbot/resource_resource.go
@@ -249,13 +249,13 @@ func formatPayload(payload string) string {
 
 func mapFromResourceData(d *schema.ResourceData, properties []string) map[string]interface{} {
 	var propertyMap = map[string]interface{}{}
-	for _, p := range properties {
+	for _, terraformProperty := range properties {
 		// get schema for property
-		value := d.Get(p)
-		// converted Terraform attribute(snake case) to lowerCamelCase.
-		var turbotAttribute = strcase.ToLowerCamel(p)
-		if value != nil {
-			propertyMap[turbotAttribute] = value;
+		value, propertySet := d.GetOk(terraformProperty)
+		if propertySet {
+			// converted property from snake case (Terraform format) to lowerCamelCase (Turbot format).
+			var turbotProperty = strcase.ToLowerCamel(terraformProperty)
+			propertyMap[turbotProperty] = value
 		}
 	}
 	return propertyMap
