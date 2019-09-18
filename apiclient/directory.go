45ad9d60d445913995e5042d0d3ac3f4eb751a7d
RupeshPatil20 <rupesh@turbot.com>
2019-09-18 11:46:53 +0530

    add Update function

--- apiclient/directory.go
+++ apiclient/directory.go
@@ -54,20 +54,33 @@ func (client *Client) ReadLocalDirectory(id string) (*LocalDirectory, error) {
 	return &responseData.Resource, nil
 }
 
-// func (client *Client) FindDirectory(title, parentAka string) ([]Directory, error) {
-// 	responseData := &FindDirectoryResponse{}
-// 	// convert parentAka into an id
-// 	parent, err := client.ReadResource(parentAka, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	parentId := parent.Turbot.Id
-// 	query := findDirectoryQuery(title, parentId)
+func (client *Client) UpdateDirectory(id, parent, title, description string) (*TurbotMetadata, error) {
+	query := updateResourceMutation()
+	responseData := &UpdateResourceResponse{}
+	var commandPayload = map[string]map[string]interface{}{
+		"data": {
+			"title":       title,
+			"description": description,
+		},
+		"turbotData": {
+			"akas": []string{id},
+		},
+	}
+	commandMeta := map[string]interface{}{
+		"typeAka":   "tmod:@turbot/turbot#/resource/types/folder",
+		"parentAka": parent,
+	}
+	variables := map[string]interface{}{
+		"command": map[string]interface{}{
+			"payload": commandPayload,
+			"meta":    commandMeta,
+		},
+	}
 
-// 	// execute api call
-// 	if err := client.doRequest(query, nil, &responseData); err != nil {
-// 		return nil, fmt.Errorf("error reading Directory: %s", err.Error())
-// 	}
+	// execute api call
+	if err := client.doRequest(query, variables, responseData); err != nil {
+		return nil, fmt.Errorf("error creating folder: %s", err.Error())
+	}
+	return &responseData.Resource.Turbot, nil
+}
 
-// 	return responseData.Directories.Items, nil
-// }
