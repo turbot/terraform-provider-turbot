b36b29a20acc6c6be7f38dd460ef4b515c80da95
RupeshPatil20 <rupesh@turbot.com>
2019-09-23 12:01:09 +0530

    add encoding func

---
+++ turbot/encoding.go
@@ -0,0 +1,34 @@
+package turbot
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"testing"
+)
+
+func EncodingTest(t *testing.T) error {
+	var requestBody bytes.Buffer
+	var queryObj = map[string]interface{}{
+		"body": "test",
+	}
+	var variables = map[string]interface{}{
+		"meta": "cool",
+	}
+	requestBodyObj := struct {
+		Query     map[string]interface{} `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Query:     queryObj,
+		Variables: variables,
+	}
+	if err := json.NewEncoder(&requestBody).Encode(requestBodyObj); err != nil {
+		fmt.Errorf("error: %s", err.Error())
+	}
+	var op interface{}
+	json.NewDecoder(&requestBody).Decode(&op)
+	log.Println("Decoded", requestBodyObj)
+	log.Println("Decoded", op)
+	return nil
+}
