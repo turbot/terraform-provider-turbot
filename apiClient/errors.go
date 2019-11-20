514ef54b548a60f419fcedfcffe191a0cc0d453a
RupeshPatil20 <rupesh@turbot.com>
2019-11-20 23:03:28 +0530

    Regex validation for Error handling. Closes #145

--- apiClient/errors.go
+++ apiClient/errors.go
@@ -3,13 +3,13 @@ package apiClient
 import "regexp"
 
 func NotFoundError(err error) bool {
-	notFoundErr := "Not Found"
+	notFoundErr := "(?i)not Found"
 	expectedErr := regexp.MustCompile(notFoundErr)
 	return expectedErr.Match([]byte(err.Error()))
 }
 
 func FailedValidationError(err error) bool {
-	dataValidationError := "data validation failed"
+	dataValidationError := "(?i)data validation failed"
 	expectedErr := regexp.MustCompile(dataValidationError)
 	return expectedErr.Match([]byte(err.Error()))
 }
