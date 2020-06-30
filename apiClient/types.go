7a1a0ea4c140414c0bf82d981419032def252762
RupeshPatil20 <rupesh@turbot.com>
2020-06-30 17:26:34 +0530

    wip

--- apiClient/types.go
+++ apiClient/types.go
@@ -372,7 +372,6 @@ type TurbotResourceMetadata struct {
 	Metadata          map[string]interface{}
 	Tags              map[string]string
 	Title             string
-	Description       string
 	VersionId         string
 	ActorIdentityId   string
 	ActorPersonaId    string
