ec6ccf3530c79fbf8caafe5bab813f9499d69c9c
RupeshPatil20 <rupesh@turbot.com>
2020-07-07 18:41:43 +0530

    add full resource query to fetch object and support to fully manage content property

--- turbot/resource_turbot_file.go
+++ turbot/resource_turbot_file.go
@@ -8,12 +8,6 @@ import (
 )
 
 var fileProperties = []interface{}{"parent", "tags", "akas"}
-var topLevelProperties = []interface{}{"title", "description"}
-
-func getFileUpdateProperties() []interface{} {
-	excludedProperties := []string{"type"}
-	return helpers.RemoveProperties(resourceProperties, excludedProperties)
-}
 
 func resourceTurbotFile() *schema.Resource {
 	return &schema.Resource{
@@ -113,21 +107,8 @@ func resourceTurbotFileCreate(d *schema.ResourceData, meta interface{}) error {
 func resourceTurbotFileRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*apiClient.Client)
 	id := d.Id()
-
-	// build required properties from content.
-	// properties is a map of property name -> property path
-
-	var properties map[string]string = nil
-
-	if _, ok := d.GetOk("content"); ok {
-		var err error = nil
-		properties, err = helpers.PropertyMapFromJson(d.Get("content").(string))
-		if err != nil {
-			return fmt.Errorf("error retrieving properties from resource data: %s", err.Error())
-		}
-	}
 	// pass nil
-	resource, err := client.ReadResource(id, properties)
+	resource, err := client.ReadFullResource(id)
 	if err != nil {
 		if apiClient.NotFoundError(err) {
 			// resource was not found - clear id
@@ -148,10 +129,15 @@ func resourceTurbotFileRead(d *schema.ResourceData, meta interface{}) error {
 	if err := storeAkas(resource.Turbot.ParentId, "parent_akas", d, meta); err != nil {
 		return err
 	}
+
+	if v, ok := customMetadata["title"]; ok {
+		d.Set("title", v)
+	}
+	if v, ok := customMetadata["description"]; ok {
+		d.Set("description", v)
+	}
 	// assign results back into ResourceData
 	d.Set("parent", resource.Turbot.ParentId)
-	d.Set("title", customMetadata["title"])
-	d.Set("description", customMetadata["description"])
 	d.Set("content", content)
 	return nil
 }
@@ -160,7 +146,8 @@ func resourceTurbotFileUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*apiClient.Client)
 	// build input map to pass to mutation
 	id := d.Id()
-	input, err := buildFileInput(d, getFileUpdateProperties())
+
+	input, err := buildFileInput(d, fileProperties)
 	if err != nil {
 		return err
 	}
@@ -206,15 +193,37 @@ func resourceTurbotFileImport(d *schema.ResourceData, meta interface{}) ([]*sche
 func buildFileInput(d *schema.ResourceData, properties []interface{}) (map[string]interface{}, error) {
 	var err error
 	var input = make(map[string]interface{})
-	var metadataMap = make(map[string]interface{})
-
+	var content = make(map[string]interface{})
 	input = mapFromResourceData(d, properties)
 	// convert data from json string to map
-	dataString := d.Get("content").(string)
-	if input["data"], err = helpers.JsonStringToMap(dataString); err != nil {
-		return nil, fmt.Errorf("error build resource mutation input, failed to unmarshal data: \n%s\nerror: %s", dataString, err.Error())
+	contentString := d.Get("content").(string)
+	if content, err = helpers.JsonStringToMap(contentString); err != nil {
+		return nil, fmt.Errorf("error build resource mutation input, failed to unmarshal content: \n%s\nerror: %s", contentString, err.Error())
+	}
+	// set old properties of content to NULL in update operation
+	if old, new := d.GetChange("content"); old != nil {
+		var oldContent, newContent map[string]interface{}
+		if oldContent, err = helpers.JsonStringToMap(old.(string)); err != nil {
+			return nil, fmt.Errorf("error build resource mutation input, failed to unmarshal content: \n%s\nerror: %s", old.(string), err.Error())
+		}
+		if newContent, err = helpers.JsonStringToMap(new.(string)); err != nil {
+			return nil, fmt.Errorf("error build resource mutation input, failed to unmarshal content: \n%s\nerror: %s", new.(string), err.Error())
+		}
+		excludeContentProperties := helpers.GetOldMapProperties(oldContent, newContent)
+		for _, key := range excludeContentProperties {
+			if _, ok := content[key.(string)]; ok {
+				content[key.(string)] = nil
+			}
+		}
 	}
 
+	input["metadata"] = buildInputMetadataMap(d)
+
+	return input, nil
+}
+
+func buildInputMetadataMap(d *schema.ResourceData) map[string]interface{} {
+	var metadataMap = make(map[string]interface{})
 	title := d.Get("title")
 	metadataMap["title"] = title
 
@@ -225,6 +234,5 @@ func buildFileInput(d *schema.ResourceData, properties []interface{}) (map[strin
 	} else {
 		metadataMap["description"] = new
 	}
-	input["metadata"] = metadataMap
-	return input, nil
+	return metadataMap
 }
