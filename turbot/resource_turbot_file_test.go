e15e876e6b02c01e71712b33ac9eaf105cd7f8cb
RupeshPatil20 <rupesh@turbot.com>
2020-07-08 18:46:54 +0530

    add new file resource. Closes #34

---
+++ turbot/resource_turbot_file_test.go
@@ -0,0 +1,117 @@
+package turbot
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-turbot/apiClient"
+	"github.com/terraform-providers/terraform-provider-turbot/helpers"
+	"testing"
+)
+
+// test suites
+func TestAccFileResourcefile_Basic(t *testing.T) {
+	resourceName := "turbot_file.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckFileResourceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFileResourceConfigfile(fileContent),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFileResourceExists(resourceName),
+					resource.TestCheckResourceAttr(
+						"turbot_file.test", "content", helpers.FormatJson(fileContent)),
+					resource.TestCheckResourceAttr(
+						"turbot_file.test", "title", "provider_file"),
+				),
+			},
+			{
+				ResourceName: resourceName,
+				ImportState:  true,
+			},
+			{
+				Config: testAccFileResourceConfigfile(fileContentDeleteKey),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFileResourceExists(resourceName),
+					resource.TestCheckResourceAttr(
+						"turbot_file.test", "content", helpers.FormatJson(fileContentDeleteKey)),
+				),
+			},
+			{
+				Config: testAccFileResourceConfigfile(fileContentUpdated),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckFileResourceExists(resourceName),
+					resource.TestCheckResourceAttr(
+						"turbot_file.test", "content", helpers.FormatJson(fileContentUpdated)),
+				),
+			},
+		},
+	})
+}
+
+var fileContent = `{
+ "foo": "provider_test",
+ "bar": "test resource"
+}
+`
+var fileContentUpdated = `{
+ "foo": "provider_test_updated",
+ "bar": "test resource"
+}
+`
+var fileContentDeleteKey = `{
+ "foo": "provider_test"
+}
+`
+
+// configs
+func testAccFileResourceConfigfile(Content string) string {
+	config := fmt.Sprintf(`
+resource "turbot_file" "test" {
+	parent = "tmod:@turbot/turbot#/"
+	title  = "provider_file"
+    description = "test"
+	content =  <<EOF
+%sEOF
+}
+`, Content)
+	return config
+}
+
+// helper functions
+func testAccCheckFileResourceExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+		client := testAccProvider.Meta().(*apiClient.Client)
+		_, err := client.ReadResource(rs.Primary.ID, nil)
+		if err != nil {
+			return fmt.Errorf("error fetching item with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckFileResourceDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*apiClient.Client)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "turbot_file" {
+			_, err := client.ReadResource(rs.Primary.ID, nil)
+			if err == nil {
+				return fmt.Errorf("Alert still exists")
+			}
+			if !apiClient.NotFoundError(err) {
+				return fmt.Errorf("expected 'not found' error, got %s", err)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ website/docs/r/file.html.markdown
@@ -0,0 +1,63 @@
+---
+layout: "turbot"
+title: "turbot"
+template: Documentation
+page_title: "Turbot: turbot_file"
+nav:
+  title: turbot_file
+---
+
+# turbot_file
+
+The `Turbot file` resource adds support for files. Turbot files define a top level hierarchy to arrange resources and their policies.
+
+## Example Usage
+
+**Creating Your First file**
+
+```hcl
+resource "turbot_file" "test" {
+  parent          = "tmod:@turbot/turbot#/"
+  title           = "My file"
+  description     = "My first test file"
+}
+```
+
+**Creating a file with content**
+
+```hcl
+resource "turbot_file" "data_file" {
+  parent          = "tmod:@turbot/turbot#/"
+  title           = "data file"
+  description     = "This is file contains data."
+  content         = <<EOF
+  {
+   "title": "provider_test",
+   "description": "test resource"
+  }EOF
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+- `content` - (Optional) Data of a file resource.
+- `description` - (Required) Brief description of the purpose and details of the file.
+- `parent` - (Required) ID or `aka` of the parent resource.
+- `title` - (Required) Short descriptive name for the file. This appears as the file name in the Turbot Console.
+- `tags` - (Optional) Labels that can be used to manage, group, categorize, search, and save metadata for this file.
+
+## Attributes Reference
+
+In addition to all the arguments above, the following attributes are exported:
+
+- `parent_akas` - A list of all akas for this file’s parent resource.
+
+## Import
+
+files can be imported using the `id`. For example,
+
+```
+terraform import turbot_file.test 123456789012
+```
