f6127f82bc268849b8b34e8363daa091fa382ace
RupeshPatil20 <rupesh@turbot.com>
2020-07-16 18:09:00 +0530

    update error message on time out operation

--- turbot/resource_turbot_mod.go
+++ turbot/resource_turbot_mod.go
@@ -163,7 +163,7 @@ func modInstall(d *schema.ResourceData, meta interface{}) error {
 			return nil
 		}
 		if err == nil {
-			err = errors.New("mod is not installed")
+			err = errors.New("turbot mod installation timed out")
 			return resource.RetryableError(err)
 		}
 		return resource.NonRetryableError(err)
