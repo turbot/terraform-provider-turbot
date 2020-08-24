214c1a726c53ae21c2a8541a20ff3396b501588b
Rupesh Patil <49144568+RupeshPatil20@users.noreply.github.com>
2020-08-24 16:29:22 +0530

    [Bug] terraform apply crashes with Index out of range error, unable to handle GRPC errors from terraform SDK. Closes #108 (#109)

--- apiClient/errors.go
+++ apiClient/errors.go
@@ -21,25 +21,40 @@ func FailedValidationError(err error) bool {
 	return expectedErr.Match([]byte(err.Error()))
 }
 
+func ExtractErrorCode(err error) (int, error) {
+	// error returned from machinebox/graphql is of graphql type
+	// errorNon200Template = "graphql: server returned a non-200 status code: 503"
+	if strings.Contains(err.Error(),"graphql") {
+		errorStringArray := strings.Split(err.Error(),":")
+		if len(errorStringArray) == 3 {
+			errCodeString := strings.TrimSpace(errorStringArray[2])
+			errCode, err := strconv.ParseUint(errCodeString, 10, 32)
+			if err != nil {
+				return 0, err
+			}
+			return int(errCode), nil
+		}
+	}
+	return 0, err
+}
+
 func BuildHttpErrorMessage(err error) error {
 	// if it's a Not Found error, we return the actual graphql error.
 	if NotFoundError(err) {
 		return err
 	}
-	errCodeString := strings.TrimSpace(strings.Split(err.Error(), ":")[2])
-	errCode, _ := strconv.ParseUint(errCodeString, 10, 32)
-
+	errCode, err := ExtractErrorCode(err)
 	// if we fail to decode the error code, just return the error directly
-	if http.StatusText(int(errCode)) == "" {
+	if http.StatusText(errCode) == "" {
 		return err
 	}
 	var errString string
 	if int(errCode) == 502 || int(errCode) == 503 || int(errCode) == 504 {
 		// retryable error codes - [502, 503, 504]
-		errString = fmt.Sprintf("The server returned a %s error (%s). Please wait a few minutes and try again.", http.StatusText(int(errCode)), errCodeString)
+		errString = fmt.Sprintf("The server returned a %s error (%v). Please wait a few minutes and try again.", http.StatusText(errCode), errCode)
 	} else {
 		// non-retryable errors
-		errString = fmt.Sprintf("The server returned a %s error (%s). Please contact Turbot support.", http.StatusText(int(errCode)), errCodeString)
+		errString = fmt.Sprintf("The server returned a %s error (%v). Please contact Turbot support.", http.StatusText(errCode), errCode)
 	}
 	return errors.New(errString)
 }
